4cf5a9b14dd56940ebffe7ea6a0a4ee2b16b5671
Alexia Maynart <alexia.maynart@gmail.com>
2024-11-28 18:11:46 +0000

    katas and tests

---
+++ tests/test_delete_nth.py
@@ -0,0 +1,11 @@
+# Importing the function I will test
+from lib.delete_nth import *
+
+
+def test_2_is_enough():
+    result = delete_nth([1,2,3,1,2,1,2,3], 2)
+    assert result == [1,2,3,1,2,3]
+
+def rest_1_is_enough():
+    result = delete_nth([20,37,20,21], 1)
+    assert result == [20,37,21]
